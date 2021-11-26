0b9ee6d83da824e753959203cd31372a43c24441
David J. Kordsmeier <dkords@gmail.com>
2021-11-25 22:14:28 -0800

    refs #2 Add first commit for Swift UI app

---
+++ CoffeeChainApp/CoffeeChainApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeChainApp
+//
+//  Created by David Kordsmeier on 11/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
